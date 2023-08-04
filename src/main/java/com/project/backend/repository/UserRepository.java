fda504a244e4ec62b102c50cabe1cfba0a66a0af
rahul8x <141213569+rahul8x@users.noreply.github.com>
2023-08-04 14:09:14 +0530

    Delete src directory

--- src/main/java/com/project/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.repository;
-
-import com.project.backend.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
